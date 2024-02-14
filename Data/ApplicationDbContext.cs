9057d4c31471393ccfafbce83c1f54c5149a6231
elyasnew <elyasesnaashari@gmail.com>
2024-02-14 21:26:36 +0330

    create a repo

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TwitterApi.Data
+{
+   public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+   {
+   }
+}
