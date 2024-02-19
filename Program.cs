97ecafcd6d752b74e61f4fdceaef5454326baee7
elyasnew <elyasesnaashari@gmail.com>
2024-02-19 21:24:51 +0330

    add identity endpoints

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Security.Claims;
 using TwitterApi.Contracts;
 using TwitterApi.Data;
 using TwitterApi.Data.Entities;
@@ -20,16 +23,44 @@ builder.Services.AddDefaultIdentity<User>(options =>
    options.Password.RequireDigit = true;
    options.Password.RequireLowercase = true;
    options.Password.RequireUppercase = true;
-}).AddEntityFrameworkStores<ApplicationDbContext>();
-//.AddApiEndpoints();
+}).AddEntityFrameworkStores<ApplicationDbContext>()
+.AddApiEndpoints();
+
+builder.Services.AddAuthentication(IdentityConstants.BearerScheme)
+                .AddBearerToken(IdentityConstants.BearerScheme);
+//builder.Services.AddAuthorizationBuilder();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IUserService, UserService>();
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+   c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+   {
+      Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+      Name = "Authorization",
+      In = ParameterLocation.Header,
+      Type = SecuritySchemeType.ApiKey,
+      Scheme = "Bearer"
+   });
+
+   c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+            },
+            new string[] {}
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -41,9 +72,13 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseRouting();
 
 app.UseAuthorization();
-//app.MapIdentityApi<User>();
+
+app.MapGroup("api/user").MapIdentityApi<User>();
+app.MapGet("api/user/claims", (ClaimsPrincipal user) => user.Identity.Name)
+   .RequireAuthorization();
 
 app.MapControllers();
 
